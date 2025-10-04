39d83d2b0c1f92fa1cb5cefb02ac27f8ca72b1a4
Amikaa01 <amitkumar798575@gmail.com>
2025-10-04 13:29:52 +0530

    Simple intrest
    program to find simple intrest

---
+++ Calculating_simple_intrest.c.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+
+int p,n;
+
+float r,si;
+
+printf("Enter the value of p = ");
+
+scanf("%d",&p);
+
+printf("Enter the value of n  = ");
+
+scanf("%d",&n);
+
+printf("Enter the vcalue of r  = ");
+
+scanf("%f",&r);
+
+si = (p*n*r)/100;
+
+printf("Simple intrest = %f\n",si);
+
+
+
+return 0;
+}
