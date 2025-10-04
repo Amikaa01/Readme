e6ef56dbb77b34e041e1f1f887ea9cfe84996ec9
Amikaa01 <amitkumar798575@gmail.com>
2025-10-04 10:13:02 +0530

    Area of the given circle
    Area of the circle

---
+++ areacirclecpp.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int radius,area;
+	cout<<"Please enter the radius of the circle:"<<endl;
+	cin>>radius;
+	area=3.14*radius*radius;
+	cout<<"Area of the circle is="<<area<<endl;
+	return 0;
+	
+}
