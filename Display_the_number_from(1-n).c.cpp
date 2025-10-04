ccccf42e88a91c84ad8b822f7b4246dc731d8c3d
Amikaa01 <amitkumar798575@gmail.com>
2025-10-04 13:36:49 +0530

    Display the number from 1  to n

---
+++ Display_the_number_from(1-n).c.cpp
@@ -0,0 +1,15 @@
+//Write a program to display numbers 1 to 10. 
+
+#include<stdio.h>
+
+int main(){
+	
+	int n,i;
+	printf("Please enter the number: ");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+		
+		printf("%d\t",i);
+	}
+	
+}
