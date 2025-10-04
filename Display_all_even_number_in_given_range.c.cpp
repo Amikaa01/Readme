2f08fca4108ee387d5a0e7ebcf195ac54c0c99a4
Amikaa01 <amitkumar798575@gmail.com>
2025-10-04 13:35:46 +0530

    Display all odd and all even number

---
+++ Display_all_even_number_in_given_range.c.cpp
@@ -0,0 +1,19 @@
+//Write a program to display all even numbers from 1 to 20 
+
+#include<stdio.h>
+
+int main(){
+	
+	int n,i;
+	printf("Please enter the number: ");
+	scanf("%d",&n);
+	
+	for(i=1;i<=n;i++)
+	{
+		if(i%2==0)
+		{
+			
+			printf("%d\t",i);
+		}
+	}
+}
