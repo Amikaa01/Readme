81c39f37850a60a12f49d9526153a6de6621fe3f
Amikaa01 <amitkumar798575@gmail.com>
2025-10-04 13:42:13 +0530

    Find number is positive or negative

---
+++ number is positive or not.c
@@ -0,0 +1,22 @@
+// Programme to check weather positive or not .
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    // Input a number
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    // Check if the number is positive, negative, or zero
+    if (num > 0) {
+        printf("%d is a positive number.\n", num);
+    } else if (num < 0) {
+        printf("%d is a negative number.\n", num);
+    } else {
+        printf("The number is zero.\n");
+    }
+
+    return 0;
+}
+
