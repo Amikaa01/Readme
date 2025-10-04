dd7502ab0ff819fda3d777f29b96d70664b72781
Amikaa01 <amitkumar798575@gmail.com>
2025-10-04 13:25:21 +0530

    Basic salary slip of an employee
    basic salaray slip

---
+++ Basic_salary_slip.c.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+
+{
+	float bp,da,hra,grpay;
+	
+	printf("Enter the value of basic pay of Amit :- ");
+	
+	scanf("%f",&bp);
+	
+	da = 0.4*bp;
+	hra = 0.2*bp;
+	grpay = da + bp + hra;
+	
+	
+	printf("Basic salary of Amit :- %f\n",bp);
+	
+	printf("Dearness allowance of Amit :- %f\n",da);
+	
+	printf("House rent allowance of Amit :- %f\n",hra);
+	
+	printf("Gross pay of Amit :- %f\n",grpay);
+	
+	return 0;
+	
+	
+	
+	
+	
+}
